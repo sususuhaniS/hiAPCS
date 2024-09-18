a0c677aa7c68c2273278495fee72d9d7b26e9950
sususuhaniS <suha.shar2000@gmail.com>
2024-09-18 14:37:09 -0700

    Add files via upload

---
+++ PigGame.java
@@ -0,0 +1,34 @@
+/**
+ *	The game of Pig.
+ *	(Description here)
+ *
+ *	@author	
+ *	@since	
+ */
+public class PigGame {
+	
+	/**	Print the introduction to the game */
+	public void printIntroduction() {
+		System.out.println("\n");
+		System.out.println("______ _         _____");
+		System.out.println("| ___ (_)       |  __ \\");
+		System.out.println("| |_/ /_  __ _  | |  \\/ __ _ _ __ ___   ___");
+		System.out.println("|  __/| |/ _` | | | __ / _` | '_ ` _ \\ / _ \\");
+		System.out.println("| |   | | (_| | | |_\\ \\ (_| | | | | | |  __/");
+		System.out.println("\\_|   |_|\\__, |  \\____/\\__,_|_| |_| |_|\\___|");
+		System.out.println("          __/ |");
+		System.out.println("         |___/");
+		System.out.println("\nThe Pig Game is human vs computer. Each takes a"
+							+ " turn rolling a die and the first to score");
+		System.out.println("100 points wins. A player can either ROLL or "
+							+ "HOLD. A turn works this way:");
+		System.out.println("\n\tROLL:\t2 through 6: add points to turn total, "
+							+ "player's turn continues");
+		System.out.println("\t\t1: player loses turn");
+		System.out.println("\tHOLD:\tturn total is added to player's score, "
+							+ "turn goes to other player");
+		System.out.println("\n");
+		
+	}
+	
+}
