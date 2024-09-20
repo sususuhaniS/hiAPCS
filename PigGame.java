353e5732d3cfe7ab0126e3b9a6b8ae8921f8bd93
sususuhaniS <suha.shar2000@gmail.com>
2024-09-20 16:39:45 -0700

    Add files via upload

--- PigGame.java
+++ PigGame.java
@@ -7,6 +7,50 @@
  */
 public class PigGame {
 	
+	private int totalScore;
+	private boolean roll;
+	private boolean userTurn;
+	private int compTot;
+	private Dice dRun;
+	
+	public PigGame()
+	{
+		totalScore = 0;
+		roll = true;
+		userTurn = true;
+		compTot = 0;
+		dRun = new Dice();
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		PigGame pg = new PigGame();
+		pg.runIt();
+	}
+	
+	public void runIt()
+	{
+		printIntroduction();
+		playGame();
+	}
+	
+	public void playGame()
+	{
+		while(totalScore <= 100 && compTot <= 100)
+		{
+			if(userTurn)
+			{
+				userGame();
+			}
+			else 
+			{
+				compGame();
+			}
+		}
+		
+	}
+	
 	/**	Print the introduction to the game */
 	public void printIntroduction() {
 		System.out.println("\n");
@@ -28,7 +72,64 @@ public class PigGame {
 		System.out.println("\tHOLD:\tturn total is added to player's score, "
 							+ "turn goes to other player");
 		System.out.println("\n");
+	}
+	
+	public void userGame()
+	{
+		int compTurnScore = 0;
+		int rollNum = 0;
+		String input = "";
+		do
+		{
+			input = Prompt.getString("**** USER Turn ***\n\nYour turn score:"
+							+ totalScore + "\nYour total score:" + totalScore + 
+							"\n\n (r)oll1 or (h)old");
+			System.out.println(input);
+			if(input.equals("r"))
+			{
+				rollNum = dRun.roll();
+				dRun.printDice();
+				if(rollNum != 1)
+				{
+					totalScore += rollNum;
+					compTurnScore += rollNum;
+				}
+				else
+				{
+					userTurn = false;
+				}
+			}
+			else
+			{
+				userTurn = false;
+			}
+		}while(userTurn);
 		
 	}
 	
+	public void compGame()
+	{
+		int rollNum = 0;
+		int compTurnScore = 0;
+		String input = "";
+		do
+		{
+			input = Prompt.getString("**** COMPUTER'S Turn ***\n" + "Computers" +
+				" turn score:" + compTurnScore + "\nComputer's total score: "
+				+ " Press enter for computer's turn");
+			rollNum = dRun.roll();
+			rollNum = dRun.roll();
+			dRun.printDice();
+			if(rollNum != 1)
+			{
+				compTot += rollNum;
+				compTurnScore += rollNum;
+				
+			}
+			else
+			{
+				userTurn = true;
+			}
+		}while(rollNum != 1);
+	}
 }
