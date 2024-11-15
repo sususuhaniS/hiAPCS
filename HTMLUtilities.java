4f2b16fccbe8805a350a3d938f29d4b3cd58c97a
sususuhaniS <suha.shar2000@gmail.com>
2024-11-15 10:34:57 -0800

    Add files via upload

--- HTMLUtilities.java
+++ HTMLUtilities.java
@@ -4,8 +4,20 @@
  *	@author	
  *	@since	
  */
-public class HTMLUtilities {
-
+public class HTMLUtilities
+{
+	
+	// NONE = not nested in a block, COMMENT = inside a comment block
+	// PREFORMAT = inside a pre-format block
+	private enum TokenState { NONE, COMMENT, PREFORMAT };
+	// the current tokenizer state
+	private TokenState state; 
+	
+	public HTMLUtilities()
+	{
+		state = TokenState.NONE;
+	}
+	
 	/**
 	 *	Break the HTML string into tokens. The array returned is
 	 *	exactly the size of the number of tokens in the HTML string.
@@ -14,133 +26,198 @@ public class HTMLUtilities {
 	 *	@param str			the HTML string
 	 *	@return				the String array of tokens
 	 */
-	public String[] tokenizeHTMLString(String str) {
-		// make the size of the array large to start
-		String[] result = new String[10000];
-		for(int i = 0; i < result.length(); i++)
-		{
-			result[i] = "";
-		}
-		 
-		int charCount = 0;
-		int tokenIzer = 0;       
-		while(charCount < str.length())
-		{
-			if(str.charAt(charCount) == '<')
-			{
-				int idexSt = str.indexOf("<");
-				int endSt = str.indexOf(">", idexSt);
-				result[tokenIzer] = str.substring(idexSt, endSt +1);
-				charCount = endSt++;
-				tokenIzer++;
-				
-			}
-			else if(isLetter(str.charAt(charCount)))
-			{
-				result[tokenIzer] += str.charAt(charCount);
-				charCount++;
-				/*String word = str.substring(str.indexOf(" ", str.charAt(charCount)),
-					str.lastIndexOf(" ", str.charAt(charCount)));
-				
-				charCount = str.indexOf(" ", str.charAt(charCount));
-				tokenIzer++;*/
-			}
-			else if(isNumber(str.substring(str.indexOf(" ", str.indexOf(charCount)),
-				str.lastIndexOf(" ", str.indexOf(charCount)))))
-			{
-				String num = str.substring(str.indexOf(" ", 
-						charCount),
-					str.lastIndexOf(" ", str.charAt(charCount)));
-				result[tokenIzer] = num;
-				charCount = str.indexOf(" ", str.charAt(charCount));
-				tokenIzer++;
-			}
-			else if(isPunctuation(str.charAt(charCount)))
-			{
-				result[tokenIzer] = " " + str.charAt(charCount);
-				tokenIzer++;
-			}
-			else if(str.charAt(charCount) == ' ')
-			{
-				charCount++;
-				tokenIzer++;
-			}
-			
-		}                                                                                                                                         
+	public String[] tokenizeHTMLString(String str)
+    {
+        String[] result = new String[10000];
+        int charCount = 0;
+        int tokenIndex = 0;
+
+        while (charCount < str.length())
+        {
+			//if there is no comment or preformat blocks
+            if (state == TokenState.NONE)
+            {
+				//checks for comments
+                if (charCount + 4 <= str.length() && str.substring
+					(charCount, charCount + 4).equals("<!--"))
+                {
+                    state = TokenState.COMMENT;
+                    int endComment = str.indexOf("-->", charCount);
+                    if (endComment != -1)
+                    {
+                        charCount = endComment + 3;
+                        state = TokenState.NONE;
+                    }
+                    else
+                    {
+                        charCount = str.length();//terminates loop
+                    }
+                }
+                
+                //checks if in pre-format blcok
+                else if (charCount + 5 <= str.length() && str.substring
+					(charCount, charCount + 5).equals("<pre>"))
+                {
+                    result[tokenIndex] = "<pre>";
+                    tokenIndex++;
+                    state = TokenState.PREFORMAT;
+                    charCount += 5;
+                }
+                //checks for html
+                else if (str.charAt(charCount) == '<')
+                {
+                    int endIdx = str.indexOf(">", charCount);
+                    if (endIdx != -1)
+                    {
+                        result[tokenIndex] = str.substring(charCount, endIdx + 1);
+                        tokenIndex++;
+                        charCount = endIdx + 1;
+                    }
+                    else
+                    {
+                        charCount = str.length(); 
+                    }
+                }
+                //checks for words
+                else if (isLetterOrHyphen(str.charAt(charCount)))
+                {
+                    String token = "";
+                    while (charCount < str.length() && isLetterOrHyphen
+						(str.charAt(charCount)))
+                    {
+                        token += str.charAt(charCount);
+                        charCount++;
+                    }
+                    result[tokenIndex] = token;
+                    tokenIndex++;
+                }
+                //checks for numbers
+                else if (isNumberStart(str.charAt(charCount)))
+                {
+                    String token = "";
+                    if (str.charAt(charCount) == '-')
+                    {
+                        token += '-';
+                        charCount++;
+                    }
+                    while (charCount < str.length() && isNumber(str.charAt
+						(charCount)))
+                    {
+                        token += str.charAt(charCount);
+                        charCount++;
+                    }
+                    if (charCount < str.length() && str.charAt(charCount) == '.')
+                    {
+                        token += '.';
+                        charCount++;
+                        while (charCount < str.length() && isNumber(str.charAt(charCount)))
+                        {
+                            token += str.charAt(charCount);
+                            charCount++;
+                        }
+                    }
+                    if (charCount < str.length() && (str.charAt(charCount) == 'e' 
+						|| str.charAt(charCount) == 'E'))
+                    {
+                        token += 'e';
+                        charCount++;
+                        if (charCount < str.length() && (str.charAt(charCount) 
+							== '-' || str.charAt(charCount) == '+'))
+                        {
+                            token += str.charAt(charCount);
+                            charCount++;
+                        }
+                        while (charCount < str.length() && 
+							isNumber(str.charAt(charCount)))
+                        {
+                            token += str.charAt(charCount);
+                            charCount++;
+                        }
+                    }
+                    result[tokenIndex] = token;
+                    tokenIndex++;
+                }
+                else if (isPunctuation(str.charAt(charCount)))
+                {
+                    result[tokenIndex] = String.valueOf(str.charAt(charCount));
+                    tokenIndex++;
+                    charCount++;
+                }
+                else
+                {
+                    charCount++;
+                }
+            }
+            //if the state is preformat, save the entire string in token
+            else if (state == TokenState.PREFORMAT)
+            {
+                    result[tokenIndex] = str.substring(charCount);
+                    tokenIndex++;
+                    charCount = str.length(); 
+            }
+            //if in comment block, skips the text in comments
+            else if (state == TokenState.COMMENT)
+            {
+                int endComment = str.indexOf("-->", charCount);
+                if (endComment != -1)
+                {
+                    charCount = endComment + 3;
+                    state = TokenState.NONE;
+                }
+                else
+                {
+                    charCount = str.length(); 
+                }
+            }
+        }
 		
-		
-		/*String htmlStart = str.substring(str.indexOf("<"), str.indexOf(">"));
-		String htmlFin = str.substring(str.indexOf("</"), str.indexOf(">"));
-		
-		String htmlBlock = str.substring(htmlStart, htmlFin);
-		
-		int counterHtml = 0;
-		while(counterHtml <= str.length())
-		{
-			int idSpace = htmlBlock.indexOf(" ");
-			String word = str.substring(idSpace, str.indexOf(" ", idSpace);
-			boolean hasPuncuation = hasPunctuation(word);
-			boolean number = isNumber(word);
-			
-			if(hasPunctuation);
-			{
-				if(word.length() == 1)
-				{
-					//set result at i to punctuation
-				}
-				else
-				{
-					for(int i = 0; i < word.length(); i++)
-					{
-						if(hasPunctuation("" + word.char(i)))
-						{
-							//set result at i to punctuation
-							//set result at i to punctuation for the rest of the word
-						}
-						
-					}
-				}
-			}
-			else if(number)
-			{
-				//set result at i to number.
-			}
-		}*/
-		
-		// return the correctly sized array
-		return result;
-	}
-	
-	
-	private boolean isLetter(char charIn)
+		//creates a new array to store the valid entries.
+        String[] outputArray = new String[tokenIndex];
+        for (int i = 0; i < tokenIndex; i++)
+        {
+            outputArray[i] = result[i];
+        }
+
+        return outputArray;
+    }
+
+	/**
+	 * checks if the character is a letter or hyphen
+	 * @return true if the character is punctuation
+	 * @param charIn a specific character in the string
+	 */
+	private boolean isLetterOrHyphen(char charIn) 
 	{
-		if((charIn >= 'A' && charIn <= 'Z')||(charIn >= 'a' && charIn <= 'z'))
-		{
-			return true;
-		}
-		return false;
-		
-	}
+        return (charIn >= 'A' && charIn <= 'Z') || (charIn >= 'a' && charIn <= 'z') || charIn == '-';
+    }
 	
-	private boolean isNumber(String numIn)
+	/**
+	 * checks to see if a character is a number or the positive or negative
+	 * or has e in it
+	 * @return true if the character is punctuation
+	 * @param charIn a specific character in the string
+	 */
+	private boolean isNumberStart(char charIn) 
 	{
-		
-		boolean isNum = false;
-		int test = -1; 
-		
-		try
-		{
-			test = Integer.parseInt(numIn);
-			isNum = true;
-		}
-		
-		catch (NumberFormatException e)
-		{
-			isNum = false;
-		}
-		return isNum;
-	}
+        return (charIn >= '0' && charIn <= '9') || charIn == '.' || 
+			charIn == '-' || charIn == '+' || charIn == 'e' || charIn == 'E';
+    }
 	
+	/**
+	 * checks to see if a character is a number
+	 * @return true if the character is punctuation
+	 * @param charIn a specific character in the string
+	 */
+    private boolean isNumber(char charIn)
+    {
+        return (charIn >= '0' && charIn <= '9');
+    }
+
+	/**
+	 * checks if the specidfic characters is a punctuation
+	 * @return true if the character is punctuation
+	 * @param charIn a specific character in the string
+	 */
 	private boolean isPunctuation(char charIn)
 	{
 		
